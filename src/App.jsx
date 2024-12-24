7a10fc31bcac1ae8011143aca07d245b3f7d53fc
mansour1809 <mansour.mothna96@gmail.com>
2024-12-24 19:43:10 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import  { useState, useEffect } from 'react';
+import Register from './Register.jsx';
+import Login from './Login.jsx';
+import Profile from './Profile.jsx';
+import SystemAdmin from './SystemAdmin.jsx';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const loggedInUser = sessionStorage.getItem('user');
+    if (loggedInUser) setUser(JSON.parse(loggedInUser));
+  }, []);
+
+  const handleLogout = () => {
+    sessionStorage.removeItem('user');
+    setUser(null);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div style={{ display: 'flex', justifyContent: 'space-around', marginTop: '50px' }}>
+      {user ? (
+        user.username === 'admin' ? (
+          <SystemAdmin user={user} onLogout={handleLogout} />
+        ) : (
+          <Profile user={user} onLogout={handleLogout} />
+        )
+      ) : (
+        <>
+          <Login onLogin={setUser} />
+          <Register />
+        </>
+      )}
+    </div>
+  );
 }
 
-export default App
+export default App;
