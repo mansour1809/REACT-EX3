c5fbfe445bff2059c5d9e12264a4bfaee4fdc217
mansour1809 <mansour.mothna96@gmail.com>
2024-12-25 19:11:53 +0200

    Update Profile.jsx

--- src/Profile.jsx
+++ src/Profile.jsx
@@ -1,12 +1,13 @@
 // Profile.jsx
 import React from 'react';
 
-function Profile({ user, onLogout }) {
+function Profile() {
   return (
     <div>
-      <h1>Welcome, {user.username}</h1>
+      niiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiir
+      {/* <h1>Welcome, {user.username}</h1>
       <p>Email: {user.email}</p>
-      <button onClick={onLogout}>Logout</button>
+      <button onClick={onLogout}>Logout</button> */}
     </div>
   );
 }
