5d36bebfcb482d7ea7db9ced6a33153f7cceb0d1
mansour1809 <mansour.mothna96@gmail.com>
2024-12-25 17:15:44 +0200

    Update Register.jsx

--- src/Register.jsx
+++ src/Register.jsx
@@ -1,12 +1,24 @@
 // Register.jsx
-import React, { useState } from 'react';
+import  { useState } from 'react';
 
 function Register() {
-  const [formData, setFormData] = useState({ username: '', password: '', confirmPassword: '', email: '' });
+  
+  const [formData, setFormData] = useState({ 
+    username: '', 
+    password: '',
+    confirmPassword: '',
+    img:'',
+    firstName:'',
+    lastName:'',
+    email: '',
+    birthDate: '',
+    city: '',
+    street: '',
+    number: 0
+   });
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({ ...formData, [name]: value });
+  const handleChange = (name , e) => {
+    setFormData({ ...formData, [name]: e.target.value });
   };
 
   const handleSubmit = (e) => {
@@ -23,10 +35,17 @@ function Register() {
 
   return (
     <form onSubmit={handleSubmit}>
-      <input type="text" name="username" placeholder="Username" onChange={handleChange} required />
-      <input type="password" name="password" placeholder="Password" onChange={handleChange} required />
-      <input type="password" name="confirmPassword" placeholder="Confirm Password" onChange={handleChange} required />
-      <input type="email" name="email" placeholder="Email" onChange={handleChange} required />
+      <input type="text" name="username" placeholder="Username" onChange={(e)=>handleChange("username" , e)} required />
+      <input type="password" name="password" placeholder="Password" onChange={(e)=>handleChange("username" , e)} required />
+      <input type="password" name="confirmPassword" placeholder="Confirm Password" onChange={(e)=>handleChange("username" , e)} required />
+     <input type="file" name="img" placeholder="Profile Picture" onChange={(e)=>handleChange("username" , e)} required/>
+     <input type="text" name="firstName" placeholder="First Name" onChange={(e)=>handleChange("username" , e)} required/>
+     <input type="text" name="lastName" placeholder="Last Name" onChange={(e)=>handleChange("username" , e)} required/>
+     <input type="email" name="email" placeholder="Email" onChange={(e)=>handleChange("username" , e)} required />
+     <input type="date" name="birthDate" placeholder="Birth Date" onChange={(e)=>handleChange("username" , e)} required/>
+     <input type="text" name="city" placeholder='City' onChange={(e)=>handleChange("username" , e)}/>
+     <input type="text" name="street" placeholder='Street' onChange={(e)=>handleChange("username" , e)}/>
+     <input type="number" name="number" placeholder='Number' onChange={(e)=>handleChange("username" , e)} min={0}/>
       <button type="submit">Register</button>
     </form>
   );
