8fcd59d79571fc25cdb19af51594aae45e1d65e7
ZoharLavi <zuish53@gmail.com>
2024-12-25 17:30:18 +0200

    Update Register.jsx

--- src/Register.jsx
+++ src/Register.jsx
@@ -20,12 +20,6 @@ function Register() {
   const handleChange = (name , e) => {
     setFormData({ ...formData, [name]: e.target.value });
   };
-
-
-
-
-
-
   
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -42,16 +36,16 @@ function Register() {
   return (
     <form onSubmit={handleSubmit}>
       <input type="text" name="username" placeholder="Username" onChange={(e)=>handleChange("username" , e)} required />
-      <input type="password" name="password" placeholder="Password" onChange={(e)=>handleChange("username" , e)} required />
-      <input type="password" name="confirmPassword" placeholder="Confirm Password" onChange={(e)=>handleChange("username" , e)} required />
-     <input type="file" name="img" placeholder="Profile Picture" onChange={(e)=>handleChange("username" , e)} required/>
-     <input type="text" name="firstName" placeholder="First Name" onChange={(e)=>handleChange("username" , e)} required/>
-     <input type="text" name="lastName" placeholder="Last Name" onChange={(e)=>handleChange("username" , e)} required/>
-     <input type="email" name="email" placeholder="Email" onChange={(e)=>handleChange("username" , e)} required />
-     <input type="date" name="birthDate" placeholder="Birth Date" onChange={(e)=>handleChange("username" , e)} required/>
-     <input type="text" name="city" placeholder='City' onChange={(e)=>handleChange("username" , e)}/>
-     <input type="text" name="street" placeholder='Street' onChange={(e)=>handleChange("username" , e)}/>
-     <input type="number" name="number" placeholder='Number' onChange={(e)=>handleChange("username" , e)} min={0}/>
+      <input type="password" name="password" placeholder="Password" onChange={(e)=>handleChange("password" , e)} required />
+      <input type="password" name="confirmPassword" placeholder="Confirm Password" onChange={(e)=>handleChange("confirmPassword" , e)} required />
+     <input type="file" name="img" placeholder="Profile Picture" onChange={(e)=>handleChange("img" , e)} required/>
+     <input type="text" name="firstName" placeholder="First Name" onChange={(e)=>handleChange("firstName" , e)} required/>
+     <input type="text" name="lastName" placeholder="Last Name" onChange={(e)=>handleChange("lastName" , e)} required/>
+     <input type="email" name="email" placeholder="Email" onChange={(e)=>handleChange("email" , e)} required />
+     <input type="date" name="birthDate" placeholder="Birth Date" onChange={(e)=>handleChange("birthDate" , e)} required/>
+     <input type="text" name="city" placeholder='City' onChange={(e)=>handleChange("city" , e)}/>
+     <input type="text" name="street" placeholder='Street' onChange={(e)=>handleChange("street" , e)}/>
+     <input type="number" name="number" placeholder='Number' onChange={(e)=>handleChange("number" , e)} min={0}/>
       <button type="submit">Register</button>
     </form>
   );
